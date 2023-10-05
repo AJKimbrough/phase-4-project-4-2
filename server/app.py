a32977bd486438b6e90ebb0088f794b97ecb4df2
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-10-05 08:41:41 -0500

    working on app.py routes

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, jsonify, make_response
 from flask_restful import Resource
 
 # Local imports
@@ -17,11 +17,34 @@ from models import Product, User, ShoppingCart, Order
 def index():
     return '<h1>Project Server</h1>'
 
-class Product(Resource):
-    def get(self):
-        products = [product.to_dict() for product in Product.query.all()]
-        return products, 200
-api.add_resource(Product, '/products')
+@app.route('/products')
+def products():
+
+    products = []
+    for product in Product.query.all():
+        product_dict = {
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "image_url": product.image_url,
+        }
+        products.append(product_dict)
+
+    response = make_response(
+        jsonify(products),
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
+
+
+# class Product(Resource):
+    
+#     def get(self):
+#         products = [product.to_dict() for product in Product.query.all()]
+#         return products, 200
+# api.add_resource(Product, '/products',endpoint='products')
 
 
 if __name__ == '__main__':
