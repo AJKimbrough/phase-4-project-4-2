67796aa5067bd6fc6096cf0022993378af08049b
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-10-08 23:20:42 -0500

    Product.js work

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Modal from './Modal';
 
 function Product({ products, addToCart }) {
-    const handleAddToCart = (homeProduct) => {
-        addToCart(homeProduct); 
-        const message = `${homeProduct.name} has been added to the cart.`;
-        alert(message); 
-      };
+const handleAddToCart = (homeProduct) => {
+    addToCart(homeProduct)
+    const message = `${homeProduct.name} has been added to the cart.`
+    alert(message)
+    }
 
-    const homeProducts = products.map((homeProduct) => (
-        <div key={homeProduct.id}>
-            <p>{homeProduct.name}</p>
-            <img alt="product img" src={homeProduct.image_url} width={250} height={250} />
-            <p>${homeProduct.price}</p>
-            <>
-                {<Modal products={homeProduct} name={homeProduct.name} price={homeProduct.price} description={homeProduct.description} />}
-            </>
-            <button onClick={() => handleAddToCart(homeProduct)}>Add to Cart</button> 
-        </div>
-      ));
+const homeProducts = products.map((homeProduct) => (
+    <div key={homeProduct.id}>
+        <p>{homeProduct.name}</p>
+        <img alt="product img" src={homeProduct.image_url} width={250} height={250} />
+        <p>${homeProduct.price}</p>
+        <>
+            {<Modal products={homeProduct} name={homeProduct.name} price={homeProduct.price} description={homeProduct.description} />}
+        </>
+        <button onClick={() => handleAddToCart(homeProduct)}>Add to Cart</button> 
+    </div>
+  ));
 
 console.log(homeProducts)
 
